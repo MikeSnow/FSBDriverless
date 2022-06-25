eddcea3847092a8bea3c7d653811537413f0d9cb
Strawberryai <ag6154lk@gmail.com>
2022-06-25 12:54:00 +0200

    Initial workspace commit

---
+++ workspace/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/alan/FSBDriverless/workspace/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/alan/test_workspace/devel;/home/alan/sim/devel;/opt/ros/melodic'.split(';') if '/home/alan/test_workspace/devel;/home/alan/sim/devel;/opt/ros/melodic' != '' else []
---
+++ workspace/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH='/home/alan/FSBDriverless/workspace/devel:/home/alan/test_workspace/devel:/home/alan/sim/devel:/opt/ros/melodic:/home/driverless/sim/devel'
+export LD_LIBRARY_PATH='/home/alan/test_workspace/devel/lib:/home/alan/sim/devel/lib:/opt/ros/melodic/lib'
+export PKG_CONFIG_PATH='/home/alan/test_workspace/devel/lib/pkgconfig:/home/alan/sim/devel/lib/pkgconfig:/opt/ros/melodic/lib/pkgconfig'
+export PWD='/home/alan/FSBDriverless/workspace/build'
+export PYTHONPATH='/home/alan/test_workspace/devel/lib/python2.7/dist-packages:/home/alan/sim/devel/lib/python2.7/dist-packages:/opt/ros/melodic/lib/python2.7/dist-packages'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/alan/FSBDriverless/workspace/devel/share/common-lisp:/home/alan/test_workspace/devel/share/common-lisp:/home/alan/sim/devel/share/common-lisp'
+export ROS_PACKAGE_PATH='/home/alan/FSBDriverless/workspace/src:/home/alan/test_workspace/src:/home/alan/sim/src/eufs_sim/eufs_description:/home/alan/sim/src/eufs_msgs:/home/alan/sim/src/eufs_sim/eufs_gazebo_plugins:/home/alan/sim/src/pointcloud_to_laserscan:/home/alan/sim/src/eufs_sim/ros_can_sim:/home/alan/sim/src/eufs_sim/eufs_gazebo:/home/alan/sim/src/eufs_sim/eufs_launcher:/home/alan/sim/src/velodyne_simulator/velodyne_description:/home/alan/sim/src/velodyne_simulator/velodyne_gazebo_plugins:/home/alan/sim/src/velodyne_simulator/velodyne_simulator:/opt/ros/melodic/share'
