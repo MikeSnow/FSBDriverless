175deef4497d94c8de72041fc6f8f6e3375d9e0e
ortiwii <48752106+ortiwii@users.noreply.github.com>
2022-10-28 13:36:01 +0200

    Delete workspace directory

--- workspace/build/estimation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alan/FSBDriverless/workspace/src/estimation
-# Build directory: /home/alan/FSBDriverless/workspace/build/estimation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- workspace/build/fsb_common/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alan/FSBDriverless/workspace/src/fsb_common
-# Build directory: /home/alan/FSBDriverless/workspace/build/fsb_common
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
